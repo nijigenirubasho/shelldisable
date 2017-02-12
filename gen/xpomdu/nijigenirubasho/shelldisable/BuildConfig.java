4a3bdbd3272f7bc6c78088998a93a398324042ea
nijigenirubasho <1597232528@qq.com>
2017-02-12 11:04:23 -0600

    Delete BuildConfig.java

--- gen/xpomdu/nijigenirubasho/shelldisable/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package xpomdu.nijigenirubasho.shelldisable;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
